ba4d449e772f9cb15154d4d1190c90cf44aa54a1
ThiagoMMoura <ago10_mariano@yahoo.com.br>
2015-12-01 20:16:31 -0200

    [INC] Nova Classe ItemNF

---
+++ src/br/edu/ulbra/gde/model/ItemNF.java
@@ -0,0 +1,95 @@
+
+package br.edu.ulbra.gde.model;
+
+/**
+ *
+ * @author Thiago Moura
+ */
+public class ItemNF {
+    private int id;
+    private int idNF;
+    private int idProduto;
+    private int quantidade;
+    private float custo;
+    private NotaFiscal nf;
+    private Produto produto;
+
+    public ItemNF(int id, int idNF, int idProduto, int quantidade, float custo, NotaFiscal nf, Produto produto) {
+        this.id = id;
+        this.idNF = idNF;
+        this.idProduto = idProduto;
+        this.quantidade = quantidade;
+        this.custo = custo;
+        this.nf = nf;
+        this.produto = produto;
+    }
+    
+    public ItemNF(int id, int idNF, int idProduto, int quantidade, float custo, NotaFiscal nf) {
+        this(id,idNF,idProduto,quantidade,custo,nf,null);
+    }
+    
+    public ItemNF(int id, int idNF, int idProduto, int quantidade, float custo) {
+        this(id,idNF,idProduto,quantidade,custo,null,null);
+    }
+    
+    public ItemNF(){
+        this(0,0,0,0,0f);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdNF() {
+        return idNF;
+    }
+
+    public void setIdNF(int idNF) {
+        this.idNF = idNF;
+    }
+
+    public int getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(int idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public float getCusto() {
+        return custo;
+    }
+
+    public void setCusto(float custo) {
+        this.custo = custo;
+    }
+
+    public NotaFiscal getNf() {
+        return nf;
+    }
+
+    public void setNf(NotaFiscal nf) {
+        this.nf = nf;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+}
