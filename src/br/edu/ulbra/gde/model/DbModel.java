a7bf0b902a15fa5a9d685d188fb4cc8f2a1f8c3c
douglasangeli <douglasangeli92@gmail.com>
2015-12-02 13:50:12 -0200

    [INC]Criada a classe DbModel Todas a classes modelo devem herdar a classe DbModel para que as classes DAO gravem corretamente no banco de dados.

---
+++ src/br/edu/ulbra/gde/model/DbModel.java
@@ -0,0 +1,23 @@
+package br.edu.ulbra.gde.model;
+
+/**
+ *
+ * @author Douglas
+ */
+public class DbModel {
+
+    private int id;
+
+    public DbModel(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}
