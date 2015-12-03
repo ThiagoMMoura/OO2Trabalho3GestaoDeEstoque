dff91c6737822c43ba4abdbf38c3536daaf3c385
douglasangeli <douglasangeli92@gmail.com>
2015-12-03 02:50:58 -0200

    [UPD]Criado metodo parse na classe Data

--- src/br/edu/ulbra/gde/model/Data.java
+++ src/br/edu/ulbra/gde/model/Data.java
@@ -5,20 +5,33 @@
 package br.edu.ulbra.gde.model;
 
 import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Adiministrador
  */
 public class Data {
-    
-    public static String format(Date data){
+
+    public static String format(Date data) {
         return DateFormat.getDateInstance().format(data);
     }
-    
-    public static String formatToSQL(Date data){
+
+    public static String formatToSQL(Date data) {
         String[] s = format(data).split("/");
-        return s[2]+"-"+s[1]+"-"+s[0];
+        return s[2] + "-" + s[1] + "-" + s[0];
+    }
+
+    public static Date parse(String target) {
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            return df.parse(target);
+        } catch (ParseException ex) {
+            return null;
+        }
     }
 }
